8f6e49a528ef4a7d82faf0a60378825f37a9694f
PyroAnnatar <pyroannatar@gmail.com>
2024-08-02 12:10:51 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [nestedObjected, setNestedObject] = useState({
-    taxi: 'Ücret karşılığında yolcu taşımak için lisanslı bir araç',
+  const [nestedObject, setNestedObject] = useState({
+    taxi: "Ücret karşılığında yolcu taşımak için lisanslı bir araç",
     food: {
       sushi:
-        'Deniz ürünleri ve sebzelerle birlikte sunulan geleneksel bir Japon yemeği',
+        "Deniz ürünleri ve sebzelerle birlikte sunulan geleneksel bir Japon yemeği",
       apple: {
         Honeycrisp:
           "MAES Bahçe Araştırma Merkezi'nde geliştirilen bir elma çeşidi",
         Fuji: "Tohoku Araştırma İstasyonu'nda yetiştiriciler tarafından geliştirilen bir elma çeşidi",
       },
     },
-  })
+  });
 
   return (
-    <div style={{ margin: 'auto', width: '70%', paddingTop: 40 }}>
-      <DisplayNested nestedObjected={nestedObjected} />
+    <div style={{ margin: "auto", width: "70%", paddingTop: 40 }}>
+      <DisplayNested nestedObject={nestedObject} />
     </div>
-  )
+  );
 }
 
-const DisplayNested = ({ nestedObjected }) => {
-  return
-  // KODUNUZ BURAYA GELECEK
-}
+const DisplayNested = ({ nestedObject }) => {
+  function discombobulator(obj) {
+    return (
+      <ul className="ml-12">
+        {Object.keys(obj).map((key) =>
+          typeof obj[key] === "string" ? (
+            <li key={key} className="mt-4">
+              {key}: {obj[key]}
+            </li>
+          ) : (
+            <li key={key} className=" mt-4">
+              {key}: {discombobulator(obj[key])}
+            </li>
+          )
+        )}
+      </ul>
+    );
+  }
 
-export default App
+  return <>{discombobulator(nestedObject)}</>;
+};
+
+export default App;
